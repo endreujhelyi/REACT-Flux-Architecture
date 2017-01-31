a007fbfcbc72afbb6dc7ad5f5e4eefa440b43ddd
Endre <endreujhelyi@gmail.com>
2017-01-31 22:05:44 +0100

    routing with react-router 1.0

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-import AppActions from '../actions/app-actions';
 import Catalog from './catalog/app-catalog';
 import Cart from './cart/app-cart';
+import Template from './app-template';
+import { browserHistory, Router, Route, IndexRoute } from 'react-router';
 
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <Catalog />
-        <Cart />
-      </div>
-    );
-  }
-}
+export default () => {
+  return (
+    <Router history={browserHistory}>
+      <Route path="/" component={Template}>
+        <IndexRoute component={Catalog} />
+        <Route path="cart" component={Cart} />
+      </Route>
+    </Router>
+  );
+};
