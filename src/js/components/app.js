64dfcbf83a832df130c3bb5bef6359223bd17ce7
Endre <endreujhelyi@gmail.com>
2017-01-31 15:11:33 +0100

    app actions

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import AppActions from '../actions/app-actions';
 
 export default class App extends Component {
   render() {
-    return <h1>A Flux App</h1>;
+    return <h1
+    onClick={AppActions.addItem.bind(null, 'this is the item')}
+    >A Flux App</h1>;
   }
 }
